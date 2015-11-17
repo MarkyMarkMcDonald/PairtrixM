5b302952d51bf769de11a8bca9cecdd5c48cebf7
Mark McDonald <pair+mark.mcdonald.ga@gmail.com>
2015-11-16 22:58:33 -0500

    Backfill TeamMemberRepository tests.
    Should be able to swap this out and not have to run the application suite. Also gives confidence that we can change tests around the application without losing coverage on the repository.

---
+++ src/test/java/pairtrix/domain/TeamMembersRepositoryTest.java
@@ -0,0 +1,52 @@
+package pairtrix.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+import pairtrix.cli.InMemoryTeamMembersRepository;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.core.Is.is;
+
+public class TeamMembersRepositoryTest {
+
+    private TeamMembersRepository teamMembersRepository;
+
+    protected TeamMembersRepository setupTeamMembersRepository() {
+        return new InMemoryTeamMembersRepository();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        teamMembersRepository = setupTeamMembersRepository();
+    }
+
+    @Test
+    public void itStartsEmpty() throws Exception {
+        assertThat(teamMembersRepository.findAll(), is(empty()));
+    }
+
+    @Test
+    public void itCanHoldTeamMembers() throws Exception {
+        teamMembersRepository.addMember("Varus");
+        teamMembersRepository.addMember("Sona");
+        teamMembersRepository.addMember("Akali");
+        teamMembersRepository.addMember("Malzahar");
+        teamMembersRepository.addMember("Udyr");
+
+        List<String> teamMembers = teamMembersRepository.findAll();
+
+        assertThat(teamMembers, hasSize(5));
+        assertThat(teamMembers, hasItems(
+                "Varus",
+                "Sona",
+                "Akali",
+                "Malzahar",
+                "Udyr"
+        ));
+    }
+}
